4c3a0969fc728a8f5a559130bb66857bd37cab2c
yuanping <3209605462@qq.com>
2020-06-20 18:03:09 +0800

    最新文件下载
    Change-Id: I75785e8ad6ca5db6f96e0667fb377538d5be1440

--- Demo.py
+++ Demo.py
@@ -1,11 +1,12 @@
 import urllib3
 from bs4 import BeautifulSoup
 import re
+import time
 
 # 获取所有的链接
 # page: 页
 def fetchUrls(page):
-    baseurl = "http://www.chinatax.gov.cn/chinatax/whmanuscriptList/n810755?_isAgg=0&_pageSize=20&_template=index&_channelName=%E6%9C%80%E6%96%B0%E6%96%87%E4%BB%B6&_keyWH=wenhao&"
+    baseurl = "http://www.chinatax.gov.cn/chinatax/whmanuscriptList/n810755?_isAgg=0&_pageSize=2000&_template=index&_channelName=%E6%9C%80%E6%96%B0%E6%96%87%E4%BB%B6&_keyWH=wenhao&"
     url = baseurl + 'page=' + str(page)
     http = urllib3.PoolManager()
     r = http.request('GET', url)
@@ -29,27 +30,43 @@ def getContent(url):
     s = BeautifulSoup(html_doc, 'html.parser')
     title1 = s.find(class_='lanse')
     title2 = s.find(class_='yanse')
-    file = open('source.txt', 'a')
+
+    file = open('最新文件.txt', 'a')
     file.write('\n\n\n=============================start=================================\n\n\n')
-    file.write(title1.text)
-    file.write('\n')
-    file.write(title2.text)
-    file.write('\n')
-    file.write('链接：'+url)
-    file.write('\n')
-    content = s.find_all('p')
-    for p in content:
-        file.write(p.text)
+    try:
+        file.write(title1.text)
         file.write('\n')
+        print(title1.text)
+    except Exception: pass
 
-    file.write('\n\n\n=============================end===================================\n\n\n')
-    file.close()
+    try:
+        file.write(title2.text)
+        file.write('\n')
+        file.write('链接：'+url)
+        file.write('\n')
+    except Exception: pass
 
-    print("Done: "+url)
+    try:
+        content = s.find_all('p')
+        for p in content:
+            file.write(p.text)
+            file.write('\n')
+
+        file.write('\n\n\n=============================end===================================\n\n\n')
+        file.close()
+    except Exception: pass
+
     return
 
-hrefs = fetchUrls(1)
+i = 1
+while i <= 1:
+    hrefs = fetchUrls(i)
+    for url in hrefs:
+        # print('start: '+url)
+        getContent(url)
+        time.sleep(5)
+
+    i += 1
 
 
-# for url in hrefs:
-#     getContent(url)
+# getContent('http://www.chinatax.gov.cn/chinatax/n810341/n810755/c5149072/content.html')
